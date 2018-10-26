817a6889b3ec24e5fbc467e5be73742290411f92
ningguilin <ningguilin@ds.cn>
2018-10-26 18:32:46 +0800

    修改注释

--- SimpleApp/js/nativemodule/CircleImageView.js
+++ SimpleApp/js/nativemodule/CircleImageView.js
@@ -1,17 +1,18 @@
 'use strict';
 
-
-// import {PropTypes} from 'react';
+/**
+ * 原生视图CircleImageView对应的JS模块(使用propType来规范接口定义)
+ */
 import {requireNativeComponent, View} from 'react-native';
 import PropTypes from 'prop-types';
 
 /**
- * 自定义的原生CircleImageView对应的JS模块
+ * 定义描述组件接口的对象circleImage
  * @type {{name: string, propTypes: {src: (*), borderRadius: (*), resizeMode: *}}}
  */
 var circleImage = {
-    name: 'CircleImageView',
-    propTypes: {
+    name: 'CircleImageView',    // name
+    propTypes: {    // propTypes
         src: PropTypes.array,
         borderRadius: PropTypes.number,
         resizeMode: PropTypes.oneOf(['cover', 'contain', 'stretch','repeat','center']),
@@ -19,4 +20,7 @@ var circleImage = {
     },
 };
 
+/**
+ * 第一个参数为原生视图的名称，第二个参数是一个描述组件接口的对象
+ */
 module.exports = requireNativeComponent('CircleImageView', circleImage);
