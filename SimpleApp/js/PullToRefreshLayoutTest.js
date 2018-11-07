5815966d669bcdf1930d6fd092a68176d793ff92
ningguilin <ningguilin@ds.cn>
2018-11-07 11:15:54 +0800

    模拟网络取数据，增加一个图片文件

--- SimpleApp/js/PullToRefreshLayoutTest.js
+++ SimpleApp/js/PullToRefreshLayoutTest.js
@@ -17,22 +17,11 @@ var screen = Dimensions.get('window');
 
 export default class PullToRefreshLayoutTest extends Component {
 
-    // 取数据
-    fetchData() {
-        // 地址有可能改变，参见https://reactnative.cn/docs/0.51/sample-application-movies.html
-        let REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json';
-
-        fetch(REQUEST_URL)
-            .then(response => response.json())
-            .then(responseData => {
-                console.log(responseData.movies.length);
-                this.pulltorefresh.stopRefresh();   // 停止刷新
-            }).catch(e => {
-                console.log("错误信息", e);
-                this.setState({
-                    load: 2,
-                });
-            });
+    // 模拟网络取数据
+    stopRefresh() {
+        setTimeout(() => {
+            this.pulltorefresh.stopRefresh();   // 停止刷新
+        }, 3000);
     }
 
     render() {
@@ -41,7 +30,8 @@ export default class PullToRefreshLayoutTest extends Component {
                 <PullToRefreshLayout ref={component => { this.pulltorefresh = component }}
                     onRefresh={() => {
                         console.log('onRefresh')
-                        this.fetchData();
+                        this.stopRefresh();
+
                     }}>
                     <Image style={{ width: screen.width, height: 200 }}
                         source={require('./img/001.jpg')} />
